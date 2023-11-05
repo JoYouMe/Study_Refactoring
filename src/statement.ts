edfe505ec95015d9e1d138d88795e405944b59ca
yuum1276 <dyalsla0105@gmail.com>
2023-11-05 12:35:31 +0900

    함수 선언 바꾸기

--- src/statement.ts
+++ src/statement.ts
@@ -1,6 +1,5 @@
-import { amountFor } from "./amountFor";
 import { I_INVOICES, I_PERFORMANCE } from "./interfaces/invoice";
-import { I_PLAYS } from "./interfaces/plays";
+import { I_PLAY, I_PLAYS } from "./interfaces/plays";
 
 export function Statement(invoice: I_INVOICES, plays: I_PLAYS): string {
     let totalAmount: number = 0;
@@ -8,7 +7,30 @@ export function Statement(invoice: I_INVOICES, plays: I_PLAYS): string {
     let result: string = `청구 내역 (고객명: ${invoice.customer})\n`;
     const format = new Intl.NumberFormat("es-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
-    function playFor(aPerformance:I_PERFORMANCE){
+    function amountFor(aPerformance:I_PERFORMANCE, play:I_PLAY) {
+        let result = 0;
+    
+            switch (playFor(aPerformance).type) {
+                case "tragedy": // 비극
+                result = 40000;
+                    if (aPerformance.audience > 30) {
+                        result += 1000 * (aPerformance.audience - 30);
+                    }
+                    break;
+                case "comedy": // 희극
+                result = 30000;
+                    if (aPerformance.audience > 20) {
+                        result += 10000 + 500 * (aPerformance.audience - 20);
+                    }
+                    result += 300 * aPerformance.audience;
+                    break;
+                default:
+                    throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
+            }
+        return result
+    }
+
+    function playFor(aPerformance:I_PERFORMANCE) {
         return plays[aPerformance.playID]
     }
 
