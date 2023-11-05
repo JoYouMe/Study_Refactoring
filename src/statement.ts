05e4935adab659b936e04f3ba879fb3c97e986e0
yuum1276 <dyalsla0105@gmail.com>
2023-11-05 12:42:46 +0900

    변수 인라인하기

--- src/statement.ts
+++ src/statement.ts
@@ -1,5 +1,5 @@
 import { I_INVOICES, I_PERFORMANCE } from "./interfaces/invoice";
-import { I_PLAY, I_PLAYS } from "./interfaces/plays";
+import {I_PLAYS } from "./interfaces/plays";
 
 export function Statement(invoice: I_INVOICES, plays: I_PLAYS): string {
     let totalAmount: number = 0;
@@ -35,15 +35,14 @@ export function Statement(invoice: I_INVOICES, plays: I_PLAYS): string {
     }
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf) // switch 
 
         //포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
         if (playFor(perf).type === "comedy") volumeCredits += Math.floor(perf.audience / 5);
 
         //청구 내역을 출력한다.
-        result += `  ${playFor(perf).name} : ${format(thisAmount / 100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        result += `  ${playFor(perf).name} : ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+        totalAmount += amountFor(perf);
     }
     result += `총액 : ${format(totalAmount / 100)}\n`;
     result += `적립 포인트 : ${volumeCredits}점\n`;
