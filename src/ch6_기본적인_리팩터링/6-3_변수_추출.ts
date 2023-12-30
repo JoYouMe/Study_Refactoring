db4d9fc8a0bf4f5177276a243082590094c88c05
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 12:21:01 +0900

    6-3. 변수 추출하기_step3

--- src/ch6_기본적인_리팩터링/6-3_변수_추출.ts
+++ src/ch6_기본적인_리팩터링/6-3_변수_추출.ts
@@ -1,11 +1,9 @@
-// 2. 이 로직을 이해했다면 기본 가격을 담을 변수를 만들고 적절한 이름을 지어준다.
+// 3. 이 변수를 실제로 사용해야 하므로 원래 표현식에서 새로 만든 변수에 해당하는 부분을 교체한다.
 
 function price(order:any) {
     // 가격 (price) = 기본 가격 - 수량 할인 + 배송비
     const basePrice = order.quantity * order.itemPrice;
-    return (
-      order.quantity * order.itemPrice -
-      Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-      Math.min(order.quantity * order.itemPrice * 0.1, 100)
-    );
+    return basePrice - 
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    Math.min(order.quantity * order.itemPrice * 0.1, 100)
   }
