128ebaecd030f66b5af6abed2da0021200c1eacb
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 13:12:03 +0900

    6-5. 매개변수를 속성으로 바꾸기_step1

--- src/ch6_기본적인_리팩터링/6-5_함수_선언_변경.ts
+++ src/ch6_기본적인_리팩터링/6-5_함수_선언_변경.ts
@@ -1,21 +1,9 @@
 /**
- *  3. 다음 새 함수의 선언문과 호출문에 원하는 매개변수를 추가한다.
+ *  매개변수를 속성으로 바꾸기
  * 
- * 호출하는 곳에서 새로 추가한 매개변수를 실제로 사용하는지 확인한다.
- * 호출문을 수정하는 과정에서 실수로 새 매개변수를 빠뜨린 부분을 찾는데 도움이 된다.
+ * 1. 매개변수로 사용할 코드를 변수로 추출해둔다.
  */
 
-class Book {
-  private _reservations: any[] = [];
-
-  addReservation(customer: any) {
-    this.zzReservations(customer, false);
-  }
-
-  zzReservations(customer: any, isPriority: boolean) {
-    if (!(isPriority === true || isPriority === false)) {
-      throw new Error('isPriority should be a boolean value');
-    }
-    this._reservations.push(customer);
-  }
+function inNewEngland(aCustomer: any){
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
 }
