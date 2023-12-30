65ca889bc66916ab9565b82c2a3a0b5b9f94a410
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 12:14:10 +0900

    지역 변수를 사용할 때

--- src/ch6_기본적인_리팩터링/6-1_함수_추출.ts
+++ src/ch6_기본적인_리팩터링/6-1_함수_추출.ts
@@ -1,5 +1,5 @@
-function printOwing(invoice:any){
-    let outstanding = 0
+function printOwing(invoice:any) {
+    let outstanding = 0;
   
     printBanner(); 
   
@@ -8,21 +8,29 @@ function printOwing(invoice:any){
       outstanding += o.amount;
     }
   
-    // 마감일 (dueDate)을 기록한다.
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-  
-    printDetails(); // 세부 사항을 출력 로직을 함수로 추출
+    recordDueDate(invoice) // 마감일 설정 로직을 함수로 추출
+    printDetails(invoice, outstanding); // 앞의 예와 달리 지역 변수를 매개변수로 전달
+    
   }
   
-  function printBanner(){
-    console.log('***********************');
-    console.log('****** 고객 채무 ******');
-    console.log('***********************');
-    }
+  function printBanner() {
+    console.log("***********************");
+    console.log("****** 고객 채무 ******");
+    console.log("***********************");
+  }
   
-  function printDetails(){
-    console.log(`고객명: ${invoice.customer}`)
-    console.log(`채무액: ${outstanding}`)
-    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`)
-    }
+  function recordDueDate(invoice:any){
+      // 마감일 (dueDate)을 기록한다.
+      const today = Clock.today;
+      invoice.dueDate = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() + 30
+      );
+  }
+  
+  function printDetails() {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  }
