301b13463a5f91035152161d68744f4fe803bed0
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 12:26:51 +0900

    6-5. 함수 선언 바꾸기_step2,step3

--- src/ch6_기본적인_리팩터링/6-5_함수_선언_변경.ts
+++ src/ch6_기본적인_리팩터링/6-5_함수_선언_변경.ts
@@ -1,5 +1,8 @@
-// 함수 이름을 너무 축약한 예
+// 2. 먼저 함수 선언부터 수정하자.
 
-function cirum(radius:any){
+function cirumference(radius:any){
     return 2 * Math.PI * radius;
   }
+  
+  // 3. 다음으로 cirum()을 호출한 곳을 모두 찾아서 cirumference()로 바꾼다.
+  
