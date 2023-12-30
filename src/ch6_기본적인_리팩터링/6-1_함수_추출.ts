d93a8434cbe1b90682b1bbf71d9e5323082893b7
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 12:16:00 +0900

    지역 변수의 값을 변경할 때

--- src/ch6_기본적인_리팩터링/6-1_함수_추출.ts
+++ src/ch6_기본적인_리팩터링/6-1_함수_추출.ts
@@ -1,18 +1,16 @@
-// 4. 내 자바스크립트 환경은 컴파일해도 아무런 값을 출력하지 않는다. 따라서 이 단계에서는 더 이상 할 일이 없다.
-// 5. 다음으로 넘어가, 추출한 코드의 원래 자리를 새로 뽑아낸 함수를 호출하는 문장으로 교체한다.
-// 추출한 함수에서 새 값을 반환하니, 이 값을 원래 변수에 저장한다.
+// 마지막으로 반환 값의 이름을 내 코딩 스타일에 맞게 바꾼다.
 
 function printOwing(invoice:any) {
     printBanner();
-    let outstanding = calculateOutstanding(invoice) // 함수 추출 완료. 추출한 함수가 반환한 값을 원래 변수에 저장한다.
+    const outstanding = calculateOutstanding(invoice)
     recordDueDate(invoice)
     printDetails(invoice, outstanding)
   }
   
   function calculateOutstanding(invoice:any) {
-    let outstanding = 0; // 추출할 코드 복사
+    let result = 0; 
     for(const o of invoice.orders) {
-      outstanding += o.amount;
+      result += o.amount;
     }
-    return outstanding; // 수정된 값 반환
+    return result; 
   }
