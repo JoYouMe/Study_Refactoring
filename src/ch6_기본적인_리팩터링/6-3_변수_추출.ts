c314903dfac9379496e065a93980313ff7c8a25a
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 12:21:45 +0900

    6-3. 변수 추출하기_step5

--- src/ch6_기본적인_리팩터링/6-3_변수_추출.ts
+++ src/ch6_기본적인_리팩터링/6-3_변수_추출.ts
@@ -1,9 +1,9 @@
-// 3. 이 변수를 실제로 사용해야 하므로 원래 표현식에서 새로 만든 변수에 해당하는 부분을 교체한다.
+// 5. 이 변수를 실제로 사용해야 하므로 원래 표현식에서 새로 만든 변수에 해당하는 부분을 교체한다.
 
-function price(order:any) {
-    // 가격 (price) = 기본 가격 - 수량 할인 + 배송비
+  function price(order:any) {
     const basePrice = order.quantity * order.itemPrice;
-    return basePrice - 
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
+    const quantityDiscount =
+      Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+    const shipping = Math.min(basePrice * 0.1, 100);
+    return basePrice - quantityDiscount + shipping;
   }
