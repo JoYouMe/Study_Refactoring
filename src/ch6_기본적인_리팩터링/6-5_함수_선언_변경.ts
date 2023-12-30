c11246f83d324418c1301d604bf636de41c280b9
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 13:13:17 +0900

    6-5. 매개변수를 속성으로 바꾸기_step2

--- src/ch6_기본적인_리팩터링/6-5_함수_선언_변경.ts
+++ src/ch6_기본적인_리팩터링/6-5_함수_선언_변경.ts
@@ -1,9 +1,10 @@
 /**
  *  매개변수를 속성으로 바꾸기
  * 
- * 1. 매개변수로 사용할 코드를 변수로 추출해둔다.
+ * 2. 함수 추출하기로 새 함수를 만든다.
  */
 
 function inNewEngland(aCustomer: any){
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
+  const stateCode = aCustomer.address.state;
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
 }
