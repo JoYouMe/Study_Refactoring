f091bf3b21dda0a040493837350474772e5a5bd4
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 12:53:22 +0900

    6-5. 매개변수 추가하기_step2

--- src/ch6_기본적인_리팩터링/6-5_함수_선언_변경.ts
+++ src/ch6_기본적인_리팩터링/6-5_함수_선언_변경.ts
@@ -1,11 +1,17 @@
 /**
- * 매개변수 추가하기
- * 
+ *  2. addReservation()의 본문을 새로운 함수로 추출한다.
+ * 새로 추출한 함수 이름도 addReservation()이어야 하지만,
+ * 기존 함수와 이름이 같은 상태로 둘 수는 없으니 우선은 
+ * 나중에 찾기 쉬운 임시 이름을 붙인다.
  */
 
 class Book {
   _reservations: any;
   addReservation(customer:any){
+    this.zz_reservations(customer)
+  }
+
+  zz_reservations(customer:any){
     this._reservations.push(customer)
   }
 }
