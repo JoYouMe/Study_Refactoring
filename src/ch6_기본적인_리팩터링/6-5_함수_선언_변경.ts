63f772a70c36900d0368cfa62816156e3c23d1b1
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 13:39:43 +0900

    6-5. 매개변수를 속성으로 바꾸기_step5

--- src/ch6_기본적인_리팩터링/6-5_함수_선언_변경.ts
+++ src/ch6_기본적인_리팩터링/6-5_함수_선언_변경.ts
@@ -1,13 +1,9 @@
 /**
  *  매개변수를 속성으로 바꾸기
  * 
- * 2. 함수 추출하기로 새 함수를 만든다.
+ * 5. 함수 인라인하기로 기존 함수의 본문을 호출문들에 집어넣는다.
+ * 실질적으로 기존 함수 호출문을 새 함수 호출문으로 교체하는 셈이다.
  */
 
-function inNewEngland(aCustomer: any){
-  return xxNEWinNewEngland(aCustomer.address.state)
-}
+const newEnglanders = someCustomers.filter(c=>xxNEWinNewEngland(c.address.state))
 
-function xxNEWinNewEngland(stateCode:any){
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
-}
