1ad2ed98a26cd96193a0b65716bb3c3ad06aa783
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 12:17:54 +0900

    6-2. 함수 인라인

--- src/ch6_기본적인_리팩터링/6-2_함수_인라인.ts
+++ src/ch6_기본적인_리팩터링/6-2_함수_인라인.ts
@@ -1,7 +1,3 @@
-function getRating(driver:any) {
-    return moreThanFiveLateDeliveries(driver) ? 2 : 1;
-  }
-  
-  function moreThanFiveLateDeliveries(driver:any) {
-    return driver.numberOfLateDeliveries > 5;
+function getRating(driver:any){
+    return (driver.numberOfLateDeliveries > 5) ? 2 : 1
   }
