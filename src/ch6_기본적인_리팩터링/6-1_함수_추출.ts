0946ecb0a5cd81403bbb945a6e6a8628fca57bb0
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 12:10:44 +0900

    6-1. 함수 추출하기

---
+++ src/ch6_기본적인_리팩터링/6-1_함수_추출.ts
@@ -0,0 +1,8 @@
+function printOwing(invoice:any){
+    printBanner();
+    let outstanding = calculateOutstanding();
+  
+    // 세부 사항 출력
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`)
+  }
