91777927292cb74adfdc78a4a43bb3e5114c6de7
yuum1276 <dyalsla0105@gmail.com>
2023-12-30 12:22:28 +0900

    6-3. 변수 추출하기_클래스 안에서

--- src/ch6_기본적인_리팩터링/6-3_변수_추출.ts
+++ src/ch6_기본적인_리팩터링/6-3_변수_추출.ts
@@ -1,9 +1,20 @@
-// 5. 이 변수를 실제로 사용해야 하므로 원래 표현식에서 새로 만든 변수에 해당하는 부분을 교체한다.
-
-  function price(order:any) {
-    const basePrice = order.quantity * order.itemPrice;
-    const quantityDiscount =
-      Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-    const shipping = Math.min(basePrice * 0.1, 100);
-    return basePrice - quantityDiscount + shipping;
+class Order {
+    constructor(aRecord:any) {
+      this._data = aRecord;
+    }
+  
+    get quantity() {
+      return this._data.quantity;
+    }
+    get itemPrice() {
+      return this._data.itemPrice;
+    }
+  
+    get price() {
+      return (
+        this.quantity * this.itemPrice -
+        Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
+        Math.min(this.quantity * this.itemPrice * 0.1, 100)
+      );
+    }
   }
