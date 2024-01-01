ca62681251ebc9dc9944709cdef61439a7c20d53
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 18:57:16 +0900

    6-8. 매개변수 객체 만들기_step5

--- src/ch6_기본적인_리팩터링/6-8_매개변수_객체.ts
+++ src/ch6_기본적인_리팩터링/6-8_매개변수_객체.ts
@@ -1,9 +1,23 @@
 /**
- * 3. 새로 만든 객체를 readingOutsideRange()의 매개변수로 추가하도록 함수 선언을 바꾼다.
+ * 5. 이제 온도 범위를 객체 형태로 전달하도록 호출문을 하나씩 바꾼다.
  */
+
+class NumberRange {
+    private _data: any;
+    constructor(min:any, max:any){
+        this._data = {min:min, max:max}
+    }
+    get min(){return this._data.min;}
+    get max(){return this._data.max}
+}
+
+let operatingPlan:any;
+let station:any;
 
 function readingOutsideRange(station:any, min:any, max:any, range:any){
     return station.readings.filter(r => r.temp < min || r.temp > max)
 }
 
 alerts = readingOutsideRange(station, operatingPlan.temperatureFloor,operatingPlan.temperatureCeiling, null)
+
+const range = new NumberRange(operatingPlan.temperatureFloor,operatingPlan.temperatureCeiling)
