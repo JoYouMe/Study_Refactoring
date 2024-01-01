eedfb0ab931ee4e04f02695bed06b14f15be45c6
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 17:19:19 +0900

    6-7. 변수 이름 바꾸기_step2

--- src/ch6_기본적인_리팩터링/6-7_변수_이름_변경.ts
+++ src/ch6_기본적인_리팩터링/6-7_변수_이름_변경.ts
@@ -1,9 +1,10 @@
 /**
- * 1. 나는 이럴 때 주로 변수 캡슐화하기로 처리한다.
+ * 2. 그런 다음 래핑 함수들을 인라인해서 모든 호출자가 변수에 직접 접근하게 하는 방법도
+ * 있지만, 나는 별로 내켜하지 않는 방식이다. 이름ㅇ르 바꾸기 위해 캡슐화부터 해야 할 정도로
+ * 널리 사용되는 변수라면 나중을 위해서라도 함수 안에 캡슐화된 채로 두는 편이 좋다고 생각하기 때문이다.
  */
-result += `<h1>${tpHd}</h1>`
 
-setTitle(obj['articleTitle'])
+let _title = 'untitled'
 
-function title() {return tpHd;} // tpHd 변수의 게터
-function setTitle(arg){tpHd = arg} // tpHd 변수의 세터
+function title() {return _title;} 
+function setTitle(arg:any){_title = arg} 
