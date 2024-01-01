4292d158a90cf04c3919bda351613eb1ac2aca14
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 23:29:30 +0900

    6-10. 여러 함수를 변환 함수로 묶기_step2

--- src/ch6_기본적인_리팩터링/6-10_여러_함수를_변환_함수로_묶기.ts
+++ src/ch6_기본적인_리팩터링/6-10_여러_함수를_변환_함수로_묶기.ts
@@ -1,10 +1,30 @@
 /**
- * 1. 우선 입력 객체를 그대로 복사해 반환하는 변환 함수로 만든다.
- * 
- * 깊은 복사는 lodash 라이브러리가 제공하는 cloneDeep()로 처리했다.
+ * 2. 이제 변경하려는 계산 로직 중 하나를 고른다. 먼저 이 계산 로직에 측정값을 전달하기 전에
+ * 부가 정보를 덧붙이도록 수정한다.
  */
 
-function enrichReading(original: any){
-    const resutl = _.cloneDeep(origin);
-    return resutl;
+import { isAssertClause } from "typescript";
+
+function calculateBaseCharge(original: any) {
+    const result = _.cloneDeep(original);
+    // 미가공 측정값에 기본 소비량을 부가 정보로 덧붙임
+    result.baseCharge = calculateBaseCharge(result);
+    return result;
 }
+
+it('check reading unchaged', function(){
+    const baseReading = {customer: 'ivan', quantity: 10, month: 5, year: 2024}
+    const oracle = _.cloneDeep(baseReading);
+    enrichReading(baseReading);
+    isAssertClause.deepEqual(baseReading, oracle)
+})
+
+// client 1
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const baseCharge = aReading.baseCharge;
+
+// client 3
+const rawReading_3 = acquireReading();
+const aReading = enrichReading(rawReading_3);
+const basicChargeAmount = aReading.baseCharge;
