91144abfd98b0a038bf8a9682070e780b72eb5d4
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 17:12:30 +0900

    6-7. 변수 이름 바꾸기_step1

--- src/ch6_기본적인_리팩터링/6-7_변수_이름_변경.ts
+++ src/ch6_기본적인_리팩터링/6-7_변수_이름_변경.ts
@@ -1,6 +1,9 @@
-let tpHd = 'untitled'
-let result
-// 어떤 참조는 다음과 같은 변수를 읽기만 한다.
+/**
+ * 1. 나는 이럴 때 주로 변수 캡슐화하기로 처리한다.
+ */
 result += `<h1>${tpHd}</h1>`
-// 값을 수정하는 곳도 있다고 해보자
-tpHd = obj['articleTitle'];
+
+setTitle(obj['articleTitle'])
+
+function title() {return tpHd;} // tpHd 변수의 게터
+function setTitle(arg){tpHd = arg} // tpHd 변수의 세터
