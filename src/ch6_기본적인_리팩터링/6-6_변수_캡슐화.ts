8707e66e2c8c21a29d38d104b7f3cc6cfab5f1b2
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 16:16:03 +0900

    6-6. 변수 캡슐화_step4

--- src/ch6_기본적인_리팩터링/6-6_변수_캡슐화.ts
+++ src/ch6_기본적인_리팩터링/6-6_변수_캡슐화.ts
@@ -1,17 +1,15 @@
 /**
- *  3. 그런 다음 defaultOwner를 참조하는 코드를 찾아서 방금 만든 게터 함수를 호출하도록 고친다.
+ *  4. 모든 참조를 수정했다면 이제 변수의 가시 범위를 제한한다.
+ * 그러면 미처 발견하지 못한 참조가 없는지 확인할 수 있고, 나중에 수정하는 코드에서도 
+ * 이 변수에 직접 접근하지 못하게 만들 수 있다.
+ * 자바스크립트로 작성할 때는 변수와 접근자 메서드를 같은 파일로 옮기고 
+ * 접근자만 노출시키면 된다.
  */
 
-spaceship.owner = getDefaultOwner;
+let defaultOwner = { firstName: '마틴', lastName: '파울러'}
 
-// 대입문은 세터 함수로 바꾼다.
-setDefaultOwner({firstName: '마틴', lastName: '파울러'}) 
-
-function defaultOwner() {return defaultOwnerData}
-
-function getDefaultOwner() {return defaultOwner}
-
-function setDefaultOwner(arg:any){defaultOwnerData = arg}
+export function getDefaultOwner(){return defaultOwner;}
+export function setDefaultOwner(arg:any){defaultOwner = arg}
 
 
 
