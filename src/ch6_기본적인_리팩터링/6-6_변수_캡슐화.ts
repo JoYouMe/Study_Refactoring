6ce885df70755b4b1ce9ce877abfdc4f482dd99e
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 15:58:20 +0900

    6-6. 변수 캡슐화_step1

--- src/ch6_기본적인_리팩터링/6-6_변수_캡슐화.ts
+++ src/ch6_기본적인_리팩터링/6-6_변수_캡슐화.ts
@@ -1,5 +1,15 @@
+/**
+ *  1. 기본적인 캡슐화를 위해 가장 먼저 데이터를 읽고 쓰는 함수부터 정의한다.
+ */
 
 let defaultOwnerData = {firstName: '마틴', lastName: '파울러'}
 
-export function defaultOwner() {return defaultOwnerData}
-export function setDefaultOwner(arg:any){defaultOwnerData = arg}
+spaceship.owner = defaultOwner;
+
+function defaultOwner() {return defaultOwnerData}
+
+function getDefaultOwner() {return defaultOwner}
+
+function setDefaultOwner(arg:any){defaultOwnerData = arg}
+
+
