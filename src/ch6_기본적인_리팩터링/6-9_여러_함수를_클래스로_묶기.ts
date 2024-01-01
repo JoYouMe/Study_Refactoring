4fdbca56182c7b3ff7d1ac2e425e84eb59265c1c
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 20:37:10 +0900

    6-9. 여러 함수를 클래스로 묶기_step3

--- src/ch6_기본적인_리팩터링/6-9_여러_함수를_클래스로_묶기.ts
+++ src/ch6_기본적인_리팩터링/6-9_여러_함수를_클래스로_묶기.ts
@@ -1,6 +1,5 @@
 /**
- * 2. 이미 만들어져 있는 calculateBaseCharge()부터 옮기자.
- * 새 클래스를 사용하려면 데이터를 얻자마자 객체로 만들어야 한다.
+ * 3. 이어서 세금을 부과할 소비량을 계산하는 코드를 함수로 추출한다.
  */
 class Reading {
     private _customer: any;
@@ -22,21 +21,17 @@ class Reading {
     get baseCharge(){
         return baseRate(this.month, this.year) * this.quantity;
     }
-
+    get taxableCharge(){
+        return Math.max(0, this.baseCharge - taxThreshold(this.year))
+    }
     
 }
 
-// client 1
-const rawReading_1 = acquireReading();
-const aReading_1 = new Reading(rawReading_1);
-const baseCharge = aReading_1.baseCharge
-
-// client 2
-const rawReading_2 = acquireReading();
-const aReading_2 = new Reading(rawReading_2);
-const taxableCharge_2 = Math.max(0, aReading_2.baseCharge - taxThreshold(aReading_2.year))
+function taxableChargeFn(aReading:any){
+    return Math.max(0, aReading.baseCharge - taxThreshold(aReading.year))
+}
 
 // client 3
-const rawReading_3 = acquireReading();
-const aReading_3 = new Reading(rawReading_3);
-const taxableCharge_3 = aReading_3.baseCharge
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const taxableCharge = aReading.taxableCharge
