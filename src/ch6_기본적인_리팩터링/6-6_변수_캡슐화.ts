a82d90db15203be98e92922012d25443ffd13ae7
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 16:10:10 +0900

    6-6. 변수 캡슐화_step3

--- src/ch6_기본적인_리팩터링/6-6_변수_캡슐화.ts
+++ src/ch6_기본적인_리팩터링/6-6_변수_캡슐화.ts
@@ -1,15 +1,18 @@
 /**
- *  1. 기본적인 캡슐화를 위해 가장 먼저 데이터를 읽고 쓰는 함수부터 정의한다.
+ *  3. 그런 다음 defaultOwner를 참조하는 코드를 찾아서 방금 만든 게터 함수를 호출하도록 고친다.
  */
 
-let defaultOwnerData = {firstName: '마틴', lastName: '파울러'}
+spaceship.owner = getDefaultOwner;
 
-spaceship.owner = defaultOwner;
+// 대입문은 세터 함수로 바꾼다.
+setDefaultOwner({firstName: '마틴', lastName: '파울러'}) 
 
 function defaultOwner() {return defaultOwnerData}
 
 function getDefaultOwner() {return defaultOwner}
 
 function setDefaultOwner(arg:any){defaultOwnerData = arg}
+
+
 
 
