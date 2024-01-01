b8012f432989fd2fc89fc096d6d7a865b2902010
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 16:25:03 +0900

    6-6. 값 캡슐화하기

--- src/ch6_기본적인_리팩터링/6-6_변수_캡슐화.ts
+++ src/ch6_기본적인_리팩터링/6-6_변수_캡슐화.ts
@@ -1,15 +1,17 @@
-/**
- *  4. 모든 참조를 수정했다면 이제 변수의 가시 범위를 제한한다.
- * 그러면 미처 발견하지 못한 참조가 없는지 확인할 수 있고, 나중에 수정하는 코드에서도 
- * 이 변수에 직접 접근하지 못하게 만들 수 있다.
- * 자바스크립트로 작성할 때는 변수와 접근자 메서드를 같은 파일로 옮기고 
- * 접근자만 노출시키면 된다.
+/** 값 캡슐화하기
+ * 기본 캡슐화 기법은 데이터 항목을 참조하는 부분만 캡슐화한다.
+ * 대부분은 이 정도로 충분하지만, 변수뿐 아니라 변수에 담긴 내용을 변경하는 행위까지
+ * 제어할 수 있게 캡슐화하고 싶을 때도 많다.
+ * 나는 주로 게터가 데이터의 복제본을 반환하도록 수정하는 식으로 처리한다.
  */
 
-let defaultOwner = { firstName: '마틴', lastName: '파울러'}
+let defaultOwnerData = { firstName: '마틴', lastName: '파울러'};
 
-export function getDefaultOwner(){return defaultOwner;}
-export function setDefaultOwner(arg:any){defaultOwner = arg}
+export function getDefaultOwner(){
+    return Object.assign({}, defaultOwnerData)
+}
+
+export function setDefaultOwner(arg:any){defaultOwnerData = arg}
 
 
 
