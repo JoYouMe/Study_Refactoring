222103083d6f9fc4d040b9a7e611dba79e72e288
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 18:20:13 +0900

    6-8. 매개변수 객체 만들기_step1

--- src/ch6_기본적인_리팩터링/6-8_매개변수_객체.ts
+++ src/ch6_기본적인_리팩터링/6-8_매개변수_객체.ts
@@ -1,22 +1,12 @@
-// 온도 측정값을 표현하는 데이터
-const station = {
-    name: 'ZB1',
-    readings:[
-        {temp: 47, time: '2024-01-01 09:10'},
-        {temp: 47, time: '2024-01-01 09:10'},
-        {temp: 47, time: '2024-01-01 09:10'},
-        {temp: 47, time: '2024-01-01 09:10'},
-        {temp: 47, time: '2024-01-01 09:10'},
-    ]
+/**
+ * 1. 묶은 데이터를 표현하는 클래스부터 선언하자.
+ */
+
+class NumberRange {
+    private _data: any;
+    constructor(min:any, max:any){
+        this._data = {min:min, max:max}
+    }
+    get min(){return this._data.min;}
+    get max(){return this._data.max}
 }
-
-// 정상 범위를 벗어난 측정값을 찾는 함수
-function readingsOutsieRange(station:any, min:any, max:any){
-    return station.readings.filter(r => r.temp < min || r.temp > max)
-}
-
-alerts = readingsOutsieRange(station,
-    operatingPlan.temperatureFloor, // 최저 온도
-    operatingPlan.temperatureCeiling // 최고 온도
-    )
-
