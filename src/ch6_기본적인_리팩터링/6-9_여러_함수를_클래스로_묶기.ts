644adb2a70ddbf3c92561fbf53edee6722265425
yuum1276 <dyalsla0105@gmail.com>
2024-01-01 20:27:46 +0900

    6-9. 여러 함수를 클래스로 묶기_step2

--- src/ch6_기본적인_리팩터링/6-9_여러_함수를_클래스로_묶기.ts
+++ src/ch6_기본적인_리팩터링/6-9_여러_함수를_클래스로_묶기.ts
@@ -1,5 +1,6 @@
 /**
- * 1. 먼저 레코드를 클래스로 변환하기 위해 레코드를 캡슐화한다.
+ * 2. 이미 만들어져 있는 calculateBaseCharge()부터 옮기자.
+ * 새 클래스를 사용하려면 데이터를 얻자마자 객체로 만들어야 한다.
  */
 class Reading {
     private _customer: any;
@@ -18,4 +19,24 @@ class Reading {
     get quantity(){return this._quantity}
     get month(){return this._month}
     get year(){return this._year}
+    get baseCharge(){
+        return baseRate(this.month, this.year) * this.quantity;
+    }
+
+    
 }
+
+// client 1
+const rawReading_1 = acquireReading();
+const aReading_1 = new Reading(rawReading_1);
+const baseCharge = aReading_1.baseCharge
+
+// client 2
+const rawReading_2 = acquireReading();
+const aReading_2 = new Reading(rawReading_2);
+const taxableCharge_2 = Math.max(0, aReading_2.baseCharge - taxThreshold(aReading_2.year))
+
+// client 3
+const rawReading_3 = acquireReading();
+const aReading_3 = new Reading(rawReading_3);
+const taxableCharge_3 = aReading_3.baseCharge
