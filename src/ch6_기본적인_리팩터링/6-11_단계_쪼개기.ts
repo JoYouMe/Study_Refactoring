b2ddd9f46745273c3bb243b1372bc74912824550
yuum1276 <dyalsla0105@gmail.com>
2024-01-02 23:46:21 +0900

    6-11. 단계 쪼개기

---
+++ src/ch6_기본적인_리팩터링/6-11_단계_쪼개기.ts
@@ -0,0 +1,12 @@
+// 상품의 결제 금액을 계산하는 코드로 시작해보자
+
+function priceOrder(product:any, quantity:any, shippingMethod:any) {
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0)
+        * product.basePrice * product.discountRate;
+    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+        ? shippingMethod.discountFee : shippingMethod.feePerCase;
+    const shippingCost = quantity * shippingPerCase;
+    const price = basePrice - discount + shippingCost;
+    return price
+}
