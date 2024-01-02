ccf3140ff42750b2187ccfab29a239a65e728b7a
yuum1276 <dyalsla0105@gmail.com>
2024-01-02 23:48:39 +0900

    6-11. 단계 쪼개기_step6

--- src/ch6_기본적인_리팩터링/6-11_단계_쪼개기.ts
+++ src/ch6_기본적인_리팩터링/6-11_단계_쪼개기.ts
@@ -1,21 +1,25 @@
 /**
- * 5. 이제 applyShipping()에 전달되는 다양한 매개변수를 살펴보자. 
-     중간 데이터 구조로 옮기고 매개변수 목록에서 제거한다.
+ * 6. 이제 첫 번째 단계 코드를 함수로 추출하고 이 데이터 구조를 반환하게 한다.
  */
 
-     function priceOrder(product:any, quantity:any, shippingMethod:any) {
-        const basePrice = product.basePrice * quantity;
-        const discount = Math.max(quantity - product.discountThreshold, 0)
-            * product.basePrice * product.discountRate;
-        const priceData = { basePrice, quantity, discount }; // 중간 데이터 구조
-        const price = applyShipping(priceData, shippingMethod);
-        return price
-    }
-    
-    function applyShipping(priceData:any, shippingMethod:any) {
-        const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
-            ? shippingMethod.discountFee : shippingMethod.feePerCase;
-        const shippingCost = priceData.quantity * shippingPerCase;
-        const price = priceData.basePrice - priceData.discount + shippingCost;
-        return price;
-    }
+function priceOrder(product:any, quantity:any, shippingMethod:any) {
+    const priceData = calculatePricingData(product, quantity)
+    return applyShipping(priceData, shippingMethod);
+}
+
+// 첫 번째 단계를 처리하는 함수
+function calculatePricingData(product:any, quantity:any) {
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0)
+        * product.basePrice * product.discountRate;
+    return { basePrice, quantity, discount }
+
+}
+
+// 두 번째 단계를 처리하는 함수
+function applyShipping(priceData:any, shippingMethod:any) {
+    const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
+        ? shippingMethod.discountFee : shippingMethod.feePerCase;
+    const shippingCost = priceData.quantity * shippingPerCase;
+    return priceData.basePrice - priceData.discount + shippingCost;
+}
