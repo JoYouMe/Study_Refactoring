17d94ba2fb0816347ea5990ad97569c120ed70f2
yuum1276 <dyalsla0105@gmail.com>
2024-01-02 23:48:14 +0900

    6-11. 단계 쪼개기_step5

--- src/ch6_기본적인_리팩터링/6-11_단계_쪼개기.ts
+++ src/ch6_기본적인_리팩터링/6-11_단계_쪼개기.ts
@@ -1,20 +1,21 @@
 /**
- * 3. 다음으로 첫 번째 단계와 두 번째 단계가 주고받을 중간 데이터 구조를 만든다.
+ * 5. 이제 applyShipping()에 전달되는 다양한 매개변수를 살펴보자. 
+     중간 데이터 구조로 옮기고 매개변수 목록에서 제거한다.
  */
 
-function priceOrder(product:any, quantity:any, shippingMethod:any) {
-    const basePrice = product.basePrice * quantity;
-    const discount = Math.max(quantity - product.discountThreshold, 0)
-        * product.basePrice * product.discountRate;
-    const priceData = {}; // 중간 데이터 구조
-    const price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
-    return price
-}
-
-function applyShipping(priceData:any, basePrice:any, shippingMethod:any, quantity:any, discount:any) {
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
-        ? shippingMethod.discountFee : shippingMethod.feePerCase;
-    const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount + shippingCost;
-    return price;
-}
+     function priceOrder(product:any, quantity:any, shippingMethod:any) {
+        const basePrice = product.basePrice * quantity;
+        const discount = Math.max(quantity - product.discountThreshold, 0)
+            * product.basePrice * product.discountRate;
+        const priceData = { basePrice, quantity, discount }; // 중간 데이터 구조
+        const price = applyShipping(priceData, shippingMethod);
+        return price
+    }
+    
+    function applyShipping(priceData:any, shippingMethod:any) {
+        const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
+            ? shippingMethod.discountFee : shippingMethod.feePerCase;
+        const shippingCost = priceData.quantity * shippingPerCase;
+        const price = priceData.basePrice - priceData.discount + shippingCost;
+        return price;
+    }
